05ce46db586adf92eba94378521110ccafb1b3f4
mixisya <78737594+isikawahonoka@users.noreply.github.com>
2022-01-30 12:56:39 +0800

    Create name.c

---
+++ name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    	int a;
+    	printf("please in code");
+    	scanf("%d",&a);
+    	switch(a) {
+            case 4:
+                printf("yes");
+                break;
+            case 5:
+                printf("no");
+                break;
+            default:
+                printf("you are fist");
+        }
+        printf("owari");
+    	return 0;
+}
